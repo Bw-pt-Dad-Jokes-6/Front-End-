73ffd18bd318bd1e55b601dc570591a211caa5b5
Brian Hague <alphaseinor@gmail.com>
2019-12-25 19:24:17 -0600

    added joke list material-table

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import MaterialTable, {MTableToolbar} from 'material-table'
 
-import JokeCard from './JokeCard'
+//import JokeCard from './JokeCard'
 
 
 
 const JokeList = (props) => {
   const [jokes, setJokes] = useState([])
+  const [columns] = useState(
+    [
+      {field: "setup", title: "Setup"},
+      {field: "punchline", title: "Punchline"},
+      {field: "type", title: "Category"},
+      {field: "id", title: "Joke Number"}
+    ]
+    )
+
 
   useEffect(() => {
     axios
       .get("https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes/10")
       .then(res => {
-        // console.log(res.data);
+        console.log(res.data);
         setJokes(res.data);
       })
       .catch(err => {
@@ -20,11 +30,36 @@ const JokeList = (props) => {
       })
   }, [])
 
+  useEffect(() => {
+    console.log(jokes)
+  }, [jokes])
+
+  useEffect(() => {
+    console.log(columns)
+  }, [columns])
+
   return (
     <div>
-      {jokes.map(joke => {
+
+      {/* {jokes.map(joke => {
         return <JokeCard key={joke.id} setup={joke.setup} punchline={joke.punchline} type={joke.type} />
-      })}
+      })} */}
+
+      <MaterialTable
+        title={`Dad Jokes`}
+        options={{
+          dense: true,
+          exportButton: true
+        }}
+        components={{
+          Toolbar: props => <MTableToolbar {...props} />
+        }}
+        
+        columns={columns}
+
+        data={jokes}
+        
+      />
     </div>
   )
 }
